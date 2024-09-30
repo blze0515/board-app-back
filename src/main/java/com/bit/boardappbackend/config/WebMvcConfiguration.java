2f88d33322bc15752db3e55fb477bef22964a47a
blze0515 <blze0515@naver.com>
2024-09-30 11:57:11 +0900

    test code 주석

--- src/main/java/com/bit/boardappbackend/config/WebMvcConfiguration.java
+++ src/main/java/com/bit/boardappbackend/config/WebMvcConfiguration.java
@@ -9,7 +9,7 @@ public class WebMvcConfiguration implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry corsRegistry) {
         corsRegistry.addMapping("/**")
-                .allowedOrigins("http://211.188.52.160:3000")
+                .allowedOriginPatterns("*")
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "PATCH")
                 .allowedHeaders("*")
                 .allowCredentials(true)
